1c92b54a7d49251c28eae040419c0ffa31d86004
EugenioNhampossa <eugenioanhampossa@gmail.com>
2023-05-06 18:35:52 +0200

    chore: add theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter"],
+      logo: ["Montserrat"],
+    },
+    colors: {
+      "gray-light": "#5C5C5C",
+      "gray-lighter": "#C0BFBF",
+      "gray-dark": "#242424",
+      "gray-medium": "#545454",
+      "light-green": "#73F423",
+      gray: "#363835",
+      black: "#000000",
+    },
     extend: {},
   },
   plugins: [],
