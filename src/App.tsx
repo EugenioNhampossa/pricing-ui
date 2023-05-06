e52b5683bdbd67158014e2545e0b21a817bd5288
EugenioNhampossa <eugenioanhampossa@gmail.com>
2023-05-06 22:06:09 +0200

    feat: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import Prices from "./components/Prices/Prices";
@@ -9,6 +10,7 @@ function App() {
         <NavBar />
         <Header />
         <Prices />
+        <Footer />
       </div>
     </>
   );
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,22 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-between px-10 py-14 bg-[#f3f3f3] rounded-md mb-5 relative">
+      <div>
+        <div className="text-3xl font-medium">Have more questions?</div>
+        <div className="text-sx font-medium text-gray-medium">
+          Get the Best Coupons with AI:
+        </div>
+        <div className="text-sx font-medium text-gray-medium">
+          Shop More, Spend Less
+        </div>
+      </div>
+      <div className="absolute right-9 bottom-9">
+        <button className="bg-light-green font-medium rounded-full px-4 py-3 text-sm text-gray">
+          <span>Contact sales team</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
